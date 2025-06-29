fb2387879af3bfff82a1cd799b9cd1312aacd22b
rashedmern <rashedmolla541@gmail.com>
2025-06-29 13:13:38 +0600

    second day

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login_Page from './Components/login_page.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <>
+        <Login_Page />
+      
+      </>
+    </div>
+  )
+}
+
+export default App
